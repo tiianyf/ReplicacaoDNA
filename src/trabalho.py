f91a4839bb78b297b726e4e0559efe7b429e89a1
01ch01 <claudio.h1902@gmail.com>
2019-04-05 13:17:33 -0300

    'trabalho' file created

---
+++ src/trabalho.py
@@ -0,0 +1,14 @@
+"""
+VALOR: 35pts
+
+1. Ler um arquivo contendo o código genético. // até aí tá tudo tranquilo.
+
+2. Fornecer um intervalo para os valores de K, gerando um arquivo txt para cada K (O intervalo será de 7 até 9). //  até aí ok tbm.
+
+3. O arquivo de saída deve conter (um arquivo de saída para cada K): // 'dna_k=7.txt', 'dna_k=8.txt', 'dna_k=9.txt'
+    a) As sequências que mais se repetem e sua quantidade; // falta só a quantidade, tem que fazer isso aí ainda.
+    b) Quais dentre as sequências do item (a) possuem inversas e sua quantidade; // meio que já fizemos, só falta a quantidade msm.
+    c) Acrescentar a taxa de mutação 'D', a qual permite uma quantidade 'D' de erros quando as comparações são feitas;// Tá, mas o que é D???
+
+OBS: Até agora é como se tivéssemos considerado tudo como D = 0.
+"""
