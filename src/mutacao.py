36403fd480bbbf47c60812164ebe6836016f37d4
01ch01 <claudio.h1902@gmail.com>
2019-04-11 14:53:58 -0300

    now we got listas de sequencias normais!

--- src/mutacao.py
+++ src/mutacao.py
@@ -17,14 +17,32 @@ OBS: Até agora é como se tivéssemos considerado tudo como D = 0.
 """
     Pegando cada sequência de tamanho k
 """
+
+
 def pegar_sequencia(string, k):
     # quantas sequências de tamanho k existem na string
     numero_de_sequencias = len(string) - k + 1
+    lista_sequencias_normais = {}
+    lista_salva_complementar = open(
+        "../assets/resultados/lista_de_sequencias_complementares.txt", "w")
 
     for i in range(numero_de_sequencias):
         sequencia = string[i: i + k]
-        # criar_string_complementar(sequencia, string)
-        return criar_string_complementar(sequencia, string)
+        if sequencia in lista_sequencias_normais:
+            lista_sequencias_normais[sequencia] += 1
+        else:
+            lista_sequencias_normais[sequencia] = 1
+
+        criar_string_complementar(
+            sequencia, string, lista_salva_complementar)
+
+    lista_salva = open(
+        '../assets/resultados/lista_de_sequencias_normais.txt', 'w')
+
+    resultado = str(lista_sequencias_normais)
+    lista_salva.write(resultado)
+    lista_salva.close()
+    lista_salva_complementar.close()
 
 
 """
@@ -33,9 +51,10 @@ def pegar_sequencia(string, k):
     Lembrando que a complementar são as ligações das respectivas bases
     nitrogenadas, só que em ordem inversa.
 """
-def criar_string_complementar(sequencia, string):
-    complementar = ''
 
+
+def criar_string_complementar(sequencia, string, lista_salva_complementar):
+    complementar = ''
     # percorrer a sequência pra criar a string complementar, de trás pra frente
     for i in range(len(sequencia)-1, -1, -1):
         troca = ''
@@ -49,25 +68,39 @@ def criar_string_complementar(sequencia, string):
             troca = 'a'
         complementar += troca
 
-    return buscar_string_complementar(complementar, string, sequencia)
+    if buscar_string_complementar(complementar, string, sequencia, lista_salva_complementar):
+        return True
+    else:
+        return False
 
 
 """
     Verifica dentro da string se a complementar (criada na função anterior)
     já existe
 """
-def buscar_string_complementar(complementar, string, sequencia):
+
+
+def buscar_string_complementar(complementar, string, sequencia, lista_salva_complementar):
     achou = False
+
+    lista_sequencias_complementares = {}
     # percorrendo a string pra ver se acha a complementar
     for i in range(len(string)):
         if string[i: i + len(complementar)] == complementar:
-            print("Eu estive aqui")
+            if complementar in lista_sequencias_complementares:
+                lista_sequencias_complementares[complementar] += 1
+            else:
+                lista_sequencias_complementares[complementar] = 1
             achou = True
-            resultado = str("Sequência:" + sequencia + "\nComplementar:" + complementar )
-            return resultado
+            print("Sequência:" + sequencia + "\nComplementar:" + complementar)
+            return True
 
     if not achou:
-        return ""
+        return False
+
+    resultado = str(lista_sequencias_complementares)
+    resultado = eval(resultado)
+    lista_salva_complementar.write(resultado)
 
 
 def main():
@@ -77,12 +110,6 @@ def main():
 
     k = int(input("\n\tInsira aqui o tamanho da string que deseja analisar: "))
     pegar_sequencia(string, k)
-    # caminho = "../assets/resultados/k={}.txt".format(k)
-
-    # arq = open(caminho, "w")
-    # resultado = print(pegar_sequencia(string, k))
-    # arq.write(resultado)
-    # arq.close()
 
 
 if __name__ == '__main__':
